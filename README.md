2ed4c4ee55944be0204636d21476dbe2f72829c2
Ali1 <ali.m.bakir@gmail.com>
2021-03-04 00:51:31 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# PlexLocalWOL
+# Plex Local WOL (Wake-On-Lan) for Raspberry Pi and other local devices
+
+If you have a raspberry pi, why not save some energy and allow the server that hosts a Plex Media Server to go to sleep! The Raspberry Pi can act as an addition Plex server, but will work to also activate a sleeping local server via Wake-On-Lan features once a client device is connected.
+
+## Requirements
+* PHP with ext-sockets (default for ubuntu on raspberry)
+* You need to know the MAC address of the ethernet port of your powerful PC
+* Your powerful PC needs to accept Wake-On-Lan packets (https://www.howtogeek.com/70374/how-to-geek-explains-what-is-wake-on-lan-and-how-do-i-enable-it/)
+
+## Installation
+
+## Install
+```
+cd ~/
+git clone https://github.com/Ali1/PlexLocalWOL/
+```
+
+### Plex set up
+First install Plex on the raspberry pi to act as a dummy server.
+On ubuntu this can be done using apt - check the Plex website
+Or if you are using Raspbian, check this https://thepi.io/how-to-set-up-a-raspberry-pi-plex-server/
+
+Go to http://localhost:32400/web/index.html on the pi and link it to your Plex account.
+It should automatically auto-start on every reboot.
+This dummy server ensures that the Pi is kept in the loop when you are remotely trying to access Plex media.
+Now that you have a Plex server on your raspberry pi, connections to the pi  will be made every time a Plex client opens on e.g. on your TV or your phone app.
+
+Now configure the app
+```
+cd ~/PlexLocalWOL
+cp config.php.new config.php
+nano config.php
+```
+Now install as an auto-starting service
+```
+cd ~/PlexLocalWOL
+bin/install
+```
+## FAQ
+### My ethernet connection is slow. Can I use wifi and ethernet
+On windows you can enable both and WOL should work. Make sure to prioritise your wifi connection to take advantage of the speed. https://www.windowscentral.com/how-change-priority-order-network-adapters-windows-10
---
+++ src/index.php
@@ -0,0 +1,97 @@
+<?php
+
+use \Diegonz\PHPWakeOnLan\PHPWakeOnLan;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = require('config.php');
+
+try {
+    $ping = new \JJG\Ping($config['host'], 128, 2);
+}
+catch (Exception $e) {
+}
+
+$is_on = $ping->ping() !== false;
+$last_ping = $last_wol = 0;
+
+$out = array();
+$tail = exec("tail -1 \"{$config['log_file_location']}\"", $out);
+$time = \Cake\Chronos\Chronos::parse(substr($tail, 0, 25) . '00');
+$last_log = $time;
+
+echo logg('Logging Starting - PC is ' . ($is_on ? 'on' : 'off'));
+while (true) {
+    $old_is_on = $is_on;
+    $is_on = $ping->ping() !== false;
+    if ($is_on !== $old_is_on) {
+        if ($is_on) {
+            echo logg('PC Has Turned On');
+        } else {
+            echo logg('PC Has Turned Off - resetting logs');
+            $out = array();
+            $tail = exec("tail -1 \"{$config['log_file_location']}\"", $out);
+            $time = \Cake\Chronos\Chronos::parse(substr($tail, 0, 25) . '00');
+            $last_log = $time;
+        }
+    }
+    if ($is_on) {
+        echo "PC is on so sleeping for 20 seconds\n";
+        sleep(20);
+    } else {
+        $out = array();
+        $tail = exec("tail -50 \"{$config['log_file_location']}\"", $out);
+        $new_lines = false;
+        foreach($out as $k => $line) {
+            $log_time = \Cake\Chronos\Chronos::parse(substr($line, 0, 25) . '00');
+            if ($last_log->gte($log_time)) {
+                continue;
+            }
+            if (!$new_lines) {
+                echo "*** NEW LINES ***\n";
+            }
+            $new_lines = true;
+            echo $line;
+            if (strpos($line, 'Completed: [') !== false) {
+                echo logg($line);
+                try {
+                    $wol = new PHPWakeOnLan();
+                    print_r($wol->wake([$config['mac']]));
+                    echo "****\n";
+                    echo logg('WOL Sent - sleeping 15 seconds and ignoring log entries');
+                    echo "****\n";
+                    sleep(15);
+                    $out = array();
+                    $tail = exec("tail -1 \"{$config['log_file_location']}\"", $out);
+                    $time = \Cake\Chronos\Chronos::parse(substr($tail, 0, 25) . '00');
+                    $last_log = $time;
+                    break;
+                } catch (Exception $e) {
+                    echo logg('Error sending WOL - ' . $e->getMessage());
+                    break;
+                }
+            }
+            echo $line . "\n";
+        }
+        if (!$new_lines) {
+            echo 'Pulled ' . count($out) . " lines from log file - no news\n";
+        } else {
+            echo "********\n";
+        }
+        $out = array_reverse($out);
+        if ($out) {
+            $last_log = \Cake\Chronos\Chronos::parse(substr($out[0], 0, 25) . '00');
+        }
+        sleep(2);
+    }
+}
+
+
+function logg($message) {
+    global $config;
+    if ($config['http_logger']) {
+        file_get_contents($config['http_logger'] . urlencode($message));
+        return "Logged: $message\n";
+    }
+    return "$message\n";
+}
